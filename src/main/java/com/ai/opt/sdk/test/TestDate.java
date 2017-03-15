248c2bffec7734f6448f98baab7d5a0d70fee366
gaogang <gaogang@asiainfo.com>
2017-03-15 15:15:43 +0800

    修改数据测试

--- src/main/java/com/ai/opt/sdk/test/TestDate.java
+++ src/main/java/com/ai/opt/sdk/test/TestDate.java
@@ -6,6 +6,6 @@ import com.ai.opt.sdk.util.DateUtil;
 
 public class TestDate {
 public static void main(String[] args) {
-	System.out.println(DateUtil.getTimeDifference(Timestamp.valueOf("2016-02-27 00:00:00"), Timestamp.valueOf("2016s-03-27 23:59:59")));;
+	System.out.println(DateUtil.getTimeDifference(Timestamp.valueOf("2000-02-27 00:00:00"), Timestamp.valueOf("2000-03-27 23:59:59")));;
 }
 }
