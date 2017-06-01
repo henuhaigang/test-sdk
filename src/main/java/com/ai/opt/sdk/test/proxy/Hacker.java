d0a67db93c358f2eaae2a10cf7cb7eb3fa60e2da
gaogang <gaogang@asiainfo.com>
2017-06-01 16:55:24 +0800

    动态代理方式

--- src/main/java/com/ai/opt/sdk/test/proxy/Hacker.java
+++ src/main/java/com/ai/opt/sdk/test/proxy/Hacker.java
@@ -10,19 +10,13 @@ public class Hacker implements MethodInterceptor {
 	@Override
 	public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
 
-
 		System.out.println("hacker代理开始。。。。。。。。");
-		
+
 		proxy.invokeSuper(obj, args);
-		
+
 		System.out.println("hacker代理结束。。。。。。。。");
-		
+
 		return null;
 	}
-
-
-
-	
-	
 
 }
